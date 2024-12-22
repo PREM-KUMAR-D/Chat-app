b542a5a833c3fc53cea25da65caf7650384bd3bc
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-12-22 11:42:22 +0530

    mongo db setup completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,20 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require('dotenv');
+
+const db = require('./lib/db');
 const authRoutes = require('./routes/auth-routes');
 
-const app = express();
 
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes )
 
 
 
-app.listen(5001,()=>{
-    console.log("Server started ...")
+app.listen(PORT,()=>{
+    console.log("Server started ... at port ", PORT);
+    db.connectDB()
 })

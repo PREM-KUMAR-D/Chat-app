86305739be9049302e4cd1b0785878daee6e3ccb
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-12-22 11:24:43 +0530

    auth routes and controller init

---
+++ Backend/src/controllers/auth-controller.js
@@ -0,0 +1,11 @@
+exports.signUp =  (req, res, next) => {
+    return res.status(200).send("working lol...")
+}
+
+exports.login =  (req, res, next) => {
+    return res.status(200).send("working lol...")
+}
+
+exports.logout =  (req, res, next) => {
+    return res.status(200).send("working lol...")
+}
